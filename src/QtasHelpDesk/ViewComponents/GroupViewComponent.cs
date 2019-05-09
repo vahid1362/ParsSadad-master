9c92c119a6f04ba04b173785b5b81cb2d7925ae2
Vahid Daneshfar <vahid.dev1362@outlook.com>
2019-05-09 12:38:43 +0430

    Add right side menu

---
+++ src/QtasHelpDesk/ViewComponents/GroupViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QtasHelpDesk.Services.Contracts.Content;
+using QtasHelpDesk.ViewModels.Content;
+
+namespace QtasHelpDesk.ViewComponents
+{
+    public class GroupViewComponent:ViewComponent
+    {
+        private readonly IGroupService _groupService;
+
+        public GroupViewComponent(IGroupService groupService)
+        {
+            _groupService = groupService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var groupViewModels = _groupService.GetGroups().Where(x => x.ParentId == null).Select(x =>
+                new GroupViewModel()
+                {
+                    Title = x.Title,
+
+                }).ToList();
+            return View(viewName: "~/Views/Shared/Components/Groups/Default.cshtml",
+                model: groupViewModels);
+        }
+    }
+}
